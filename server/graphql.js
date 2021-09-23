0a3db4fcbe88d0434b6f4b699779829ee000fb8e
Serg Kizim <sergkizim@gmail.com>
2021-09-23 15:21:33 +0300

    2.3 - this is where the problem begins

--- server/graphql.js
+++ server/graphql.js
@@ -1,18 +1,11 @@
 import { WebApp } from 'meteor/webapp';
 
 import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import { execute, subscribe } from 'graphql';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
 
-import express from 'express';
-import http from 'http';
-
 import schema from './schema';
-
-// const app = express();
-// app.use(express.json());
-// const httpServer = http.createServer(app);
 
 const app = WebApp.connectHandlers;
 
@@ -45,8 +38,7 @@ const startApolloServer = async() => {
     path: server.graphqlPath,
  });
 
-  // await new Promise(resolve => httpServer.listen({ port: 3000 }, resolve));
-  // console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`);
+  console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`);
 }
 
-startApolloServer().then(console.log).catch(console.error);
+startApolloServer().catch(console.error);
